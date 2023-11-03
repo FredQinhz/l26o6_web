184dc5437611e8116972ff5be3ac7bdda50e63b8
hanbo <211250129@smail.nju.edu.cn>
2023-11-03 14:51:17 +0800

    Prettier test

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -19,18 +19,23 @@ const ruleForm = reactive({
 
 const rules = reactive<FormRules>({
   username: [{ required: true, message: '此字段为必填项', trigger: 'change' }, {
-    min: 4, max: 16, message: '用户名长度不符合要求(4-16)', trigger: 'change'
-  }, {
-    pattern: /^[a-z\d-_]*$/, message: '用户名只能包含小写字母,数字,下划线和连字符', trigger: 'change'
-  }],
+    min: 4,
+     max: 16,
+     message: '用户名长度不符合要求(4-16)', t
+     rigger: 'change'
+
+     
+  }, {pattern: /^[a-z\d-_]*$/, message: '用户名只能包含小写字母,数字,下划线和连字符', trigger: 'change'}],
+
+
   password: [{ required: true, message: '此字段为必填项', trigger: 'change' }, {
     min: 8, max: 56, message: '密码长度不符合要求(8-56)', trigger: 'change'
   }, {
     pattern: /^[\x21-\x7e]*$/, message: '密码只能包含字母,数字和符号', trigger: 'change'
   }, {
-    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/,
-    message: '密码未达到复杂性要求:密码必须包含大小写字母和数字',
-    trigger: 'change'
+      pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/,
+        message: '密码未达到复杂性要求:密码必须包含大小写字母和数字',
+              trigger: 'change'
   }],
 })
 
