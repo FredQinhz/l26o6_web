9ba036f6ffe92e66522a2cf15ccb6b55266586de
hanbo <211250129@smail.nju.edu.cn>
2023-11-03 22:30:53 +0800

    add backend url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // const SERVER_ADDR = "https://mock.apifox.cn/m1/2555088-0-default"
-const SERVER_ADDR = "http://localhost:8080"
+const SERVER_ADDR = "http://172.29.4.124:8080"
 export { SERVER_ADDR }
